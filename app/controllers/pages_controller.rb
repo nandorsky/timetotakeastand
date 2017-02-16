f3a091a2dd29c75626b16c4c910e45e31628ce9a
Nate Andorsky <nandorsky@gmail.com>
2017-02-16 10:32:54 -0500

    fixed comments issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
   	@stands = Stand.all
   	@two_minutes = Stand.where(time: "2 Minutes", status: "active")
-  	@one_hour = Stand.where(time: "1 Hour")
-  	@one_day = Stand.where(time: "1 Day")
-  	@multiple_days = Stand.where(time: "Multiple Days")
+  	@one_hour = Stand.where(time: "1 Hour", status: "active")
+  	@one_day = Stand.where(time: "1 Day", status: "active")
+  	@multiple_days = Stand.where(time: "Multiple Days", status: "active")
   end
 end
